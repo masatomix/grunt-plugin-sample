5b420242d040884804300d65da45edfacf957e09
Masatomi KINO <masatomix@ki-no.org>
2017-03-14 21:08:38 +0900

    プラグイン作成に最低限必要な要素を新規追加。

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,13 +15,28 @@ module.exports = function(grunt) {
                 src: 'src/<%= pkg.name %>.js',
                 dest: 'build/<%= pkg.name %>.min.js'
             }
-        }
+        },
+        hello: {
+            develop: { // registerMultiTask の場合は、ここまでは定義が必要っぽい。
+                src: 'source path',
+                dest: 'destination path',
+            }
+        },
+
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
+    grunt.loadTasks('grunt');
+
     // Default task(s).
     grunt.registerTask('default', ['uglify']);
+
+
+    grunt.registerTask('mytask', [
+        'uglify',
+        'hello'
+    ]);
 
 };
